ebee4e898955fe4b91fa925aa41504edf8c9a2f0
WENDY ESTUPIÑAN <westupinan@Mac-mini-de-Administrator-2.local>
2019-05-16 17:24:19 -0500

    agregar estilos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,86 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css?v=1.4') }}" rel="stylesheet" type="text/css"/>
-        <link rel="shortcut icon" href="{{{ asset('img/People-Doctor-Female-icon.png') }}}"> 
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+            href="{{ asset('css/app.css?v=1.4') }}"
+            rel="stylesheet"
+            type="text/css"
+        />
+        <link
+            rel="shortcut icon"
+            href="{{{ asset('img/People-Doctor-Female-icon.png') }}}"
+        />
 
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet"
+        />
     </head>
     <body>
-    <div class="container-fluid">
-    <div class="row">
-    </div>
-    <div class="header_top">
-    <div class="row">
-            <!--<img height="70" class="mr-3 promo" src="{{asset('img/logo.png')}}?v=2" alt=""/>-->
-    </div>
-     </div>
-     <div class="header_top2">
-     </div>
-     <div class="header_top3">
-        <div class="row">
-            <div class="col">
-            hola
+        <div>
+            <div class="header_container">
+                <div class="header_top">
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-2">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="header_top2"></div>
+                <div class="header_top3">
+                    <div class="row">
+                        <div class="col">
+                                <img
+                                height="50"
+                                class="mr-3 promo"
+                                src="{{ asset('img/iconos/home.png') }}?v=2"
+                                alt=""
+                            />
+
+                            <img
+                                height="50"
+                                class="mr-3 promo"
+                                src="{{ asset('img/iconos/what.png') }}?v=2"
+                                alt=""
+                            />
+                            <img
+                                height="50"
+                                class="mr-3 promo"
+                                src="{{ asset('img/iconos/requisitos.png') }}?v=2"
+                                alt=""
+                            />
+                            <img
+                                height="50"
+                                class="mr-3 promo"
+                                src="{{ asset('img/iconos/procesos.png') }}?v=2"
+                                alt=""
+                            />
+                            <img
+                                height="50"
+                                class="mr-3 promo"
+                                src="{{ asset('img/iconos/preguntas.png') }}?v=2"
+                                alt=""
+                            />
+                        </div>
+                        <div class="col"></div>
+                    </div>
+                </div>
             </div>
-            <div class="col">
-            prueba
-            </div>
+            <div class="title m-b-md"></div>
         </div>
-     </div>
-     <div class="title m-b-md">
-      Laravel
-     </div>
-     container </div>
+
+        <div class="container-fluid">
+            <div class="row"></div>
+        </div>
+        <div class="footer  text-center">
+            &copy Derechos reservados.
+        </div>
+    </div>
     </body>
 </html>
